bb6b108ea1816a6ee19eaf9e4e9565ffca2a426a
kekepins <philippe.pinston@gmail.com>
2018-03-09 17:06:25 +0100

    Add unknown sports

--- src/main/java/kikstrava/model/ActivityType.java
+++ src/main/java/kikstrava/model/ActivityType.java
@@ -1,24 +1,38 @@
 package kikstrava.model;
 
 public enum ActivityType {
-    Ride("ride", 3, 4), 
+	Alpineski("alpineski", 36, 20),
+	BackcountrySki("backcountryski", 36, 20), // FIXME
+	Canoeing("canoe", 36, 20), // FIXME
+	Crossfit("crossfit", 1, 3), // FIXME
+	Ebikeride("ebikeride", 1, -1),
+	Elliptical("eliptic", 1, -1), // FIXME
+	Hike("hike", 1, 96),
+	Iceskate("iceskate", 1, 70),
+	Inlineskate("inlineskate", 1, 8),
+	Kayaking("kayak", 1, -1), // FIXME 
+	Kitesurf("kitesurf", 1, -1), // FIXME
+	NordicSki("NordicSki", 13, 22),
+	Ride("ride", 3, 4),
+	RockClimbing("rockclimbing", 1,-1), // FIXME
+	Rollerski("rollerski", 5, 88),
+	Rowing("rameur", 1, -1),
     Run("run", 1, 3), 
+    Snowboard("snowboard", 36, 21), 
+    Snowshoe("raquette",  36, 85), 
+    StairStepper("stairstepper", 1, -1),
+    StandUpPaddling("standup", 1, -1),
+    Surfing("surfing", 1, -1),
     Trail("trail", 25, 82),
     Swim("swim", 10, 6), 
     SwimExt("swim ext", 10, 83),
-    Workout("workout", 1, -1), 
-    Hike("hike", 1, 96), 
+    VirtualRide("virtualride", 3, 4),
     Walk("walk", 28, 12), 
-    NordicSki("NordicSki", 13, 22), 
-    Alpineski("alpineski", 36, 20), 
-    Iceskate("iceskate", 1, 70), 
-    Inlineskate("inlineskate", 1, 8), 
-    Rollerski("rollerski", 5, 88), 
-    Windsurf("windsurf", 1, 86), 
-    Snowboard("snowboard", 36, 21), 
-    Snowshoe("snowshoe", 36, 85), 
-    Ebikeride("ebikeride", 1, -1), 
-    Virtualride("virtualride", 1, -1);
+    WeightTraining("weight", 1, -1),
+    Windsurf("windsurf", 1, 86),
+    Workout("workout", 1, -1), 
+    Yoga("yoga", 1, -1);
+	
 
     private String code;
     private int kikCode;
